e916633e93605f045f59e0ccba1e1f8bd5ee529f
phsr <phasor@phsr.org>
2010-11-15 20:58:40 -0500

    Updated the time entry patch and form hook to handle the case when the user does not have a Freshbooks_api_key set

--- lib/time_entry_patch.rb
+++ lib/time_entry_patch.rb
@@ -10,8 +10,7 @@ module RedmineFreshbooks
         # Same as typing in the class 
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
-          attr_accessor :send_to_freshbooks
-          attr_accessor :freshbooks_project_permitted
+          attr_accessor :send_to_freshbooks, :freshbooks_project_permitted
           
           alias_method_chain :after_initialize, :set_send_to_freshbooks
         end
@@ -24,12 +23,18 @@ module RedmineFreshbooks
 
       module InstanceMethods
         def after_initialize_with_set_send_to_freshbooks
-          after_initialize_without_set_send_to_freshbooks
-          if @project
-            @freshbooks_project_permitted = @project.freshbooks_project.freshbooks_staff_members.include?(User.current.freshbooks_staff_member)
-            Rails.logger.debug "Allowed = " + @freshbooks_project_permitted.to_s
-            @send_to_freshbooks = User.current.log_to_freshbooks && @freshbooks_project_permitted
+          @send_to_freshbooks = false
+          @freshbooks_project_permitted = false
+          Rails.logger.debug "Good before user call"
+          unless User.current.freshbooks_api_key.nil? || User.current.freshbooks_api_key.empty?
+            Rails.logger.debug "Good before @project"
+            if @project
+              @freshbooks_project_permitted = @project.freshbooks_project.freshbooks_staff_members.include?(User.current.freshbooks_staff_member)
+              @send_to_freshbooks = User.current.log_to_freshbooks && @freshbooks_project_permitted
+            end
           end
+          after_initialize_without_set_send_to_freshbooks
+          
         end
       end
   end
